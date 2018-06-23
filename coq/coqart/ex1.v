5fc52871c178a619e73cc3bd662aadef29737c3c
Newaskre <newaskre@protonmail.ch>
2018-06-23 19:22:02 +0700

    ~

---
+++ coq/coqart/ex1.v
@@ -0,0 +1,17 @@
+Require Import Arith.
+Require Import ZArith.
+Require Import Bool.
+
+Locate "_ * _".
+Print Scope Z_scope.
+
+Check 33%nat.
+Check 0%nat.
+
+Check 0%Z.
+Check (-12)%Z.
+
+Check true.
+Check false.
+
+Check ifb.
